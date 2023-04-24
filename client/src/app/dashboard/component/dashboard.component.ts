a9aa0f84b91fdc7c012c1acaaad6b3d7298a1cc1
lutz <gabriel.lutz5@gmail.com>
2023-04-24 10:21:38 -0300

    update@client: components > component

---
+++ client/src/app/dashboard/component/dashboard.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from "@angular/core"
+import { LineChartData, PieChartData } from "src/app/@types/ChartConfig"
+import { DashboardService } from "../dashboard.service"
+
+@Component({
+	selector: "app-dashboard",
+	templateUrl: "./dashboard.component.html",
+	styleUrls: ["./dashboard.component.css"],
+})
+export class DashboardComponent implements OnInit {
+	protected height = "30em"
+
+	protected pieChartData = {} as PieChartData
+
+	protected lineChartData = {} as LineChartData
+
+	constructor(private dashService: DashboardService) {}
+
+	ngOnInit(): void {
+		this.dashService.lancamentosPorCategoria().then((data) => {
+			console.log(data)
+			this.pieChartData = data
+		})
+
+		this.dashService.lancamentosPorDia().then((data) => {
+			this.lineChartData = data
+		})
+	}
+}
